bc8af287cf7d88527de80391e867d27e1754f9d4
Skipper047 <Ski_pper@bk.ru>
2022-03-21 03:38:14 +0600

    with dop

---
+++ src/com/company/Skeleton.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Skeleton extends Boss{
+    private int arrows;
+    public Skeleton(int health, int damage, int arrows){
+        super(health, damage);
+        this.arrows=arrows;
+    }
+    public String printInfo(){return super.printInfo()+" "+"Arrows: "+arrows;}
+
+
+
+    public int getArrows() { return arrows; }
+
+    public void setArrows(int arrows) { this.arrows = arrows; }
+
+
+}
